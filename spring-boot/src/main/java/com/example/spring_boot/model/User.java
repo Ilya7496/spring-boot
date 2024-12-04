bb5a7ba652909b99c2d42270d78a18bc2fbe97de
ilyamikhailov <79221803601me@gmail.com>
2024-12-04 18:24:46 +0300

    Создал интерфейс UserService Добавил Валидацию

--- spring-boot/src/main/java/com/example/spring_boot/model/User.java
+++ spring-boot/src/main/java/com/example/spring_boot/model/User.java
@@ -1,19 +1,32 @@
 package com.example.spring_boot.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Имя не должно быть пустым")
+    @Pattern(regexp = "^[a-zA-Zа-яА-ЯёЁ]+$", message = "Имя может содержать только русские или английские буквы")
     @Column(name = "name")
     private String name;
+
+    @NotBlank(message = "Email не должен быть пустым")
+    @Email(message = "Некорректный формат email")
     @Column(name = "Email")
     private String email;
+
+    @NotBlank(message = "Телефон не должен быть пустым")
+    @Pattern(regexp = "^[0-9\\-\\+]{11}$", message = "Телефон должен содержать 11 цифр")
     @Column(name = "Phone")
     private String phone;
 }
--- spring-boot/src/main/java/com/example/spring_boot/service/UserServiceImp.java
+++ spring-boot/src/main/java/com/example/spring_boot/service/UserServiceImp.java
@@ -6,22 +6,26 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-public class UserService {
+public class UserServiceImp implements UserService {
 private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserServiceImp(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    @Override
     public User findById(Long id) {
         return userRepository.getOne(id);
     }
+    @Override
     public List<User> findAll() {
         return userRepository.findAll();
     }
+    @Override
     public User saveUser(User user) {
         return userRepository.save(user);
     }
+    @Override
     public void deleteById(Long id) {
         userRepository.deleteById(id);
     }
